f4dc746934e5e7cc2776ebffbae3d33d075bf507
Akshaybagai52 <akshaybagai52@gmail.com>
2024-11-18 22:56:00 +0530

    feat: added mututated string

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,23 @@ fn main() {
 
     let is_family = true;
     if is_family {
-
         println!("is: {}", is_family )
     }
+
+    let sentence = String::from("My name is Akshay");
+    let first_word = get_first_word(sentence);
+
+    println!("first word : {}", first_word)
+}
+
+fn get_first_word(sentence: String) -> String {
+    let mut ans = String::from("");
+    for char in sentence.chars() {
+        ans.push_str(char.to_string().as_str());
+        if char == ' ' {
+            break;
+        }
+    }
+    return  ans;
+
 }
