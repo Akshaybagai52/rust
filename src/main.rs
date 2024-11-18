3f55b859fcca1384cbaf1f64cb84987b3f68abf1
Akshaybagai52 <akshaybagai52@gmail.com>
2024-11-18 22:22:11 +0530

    feat: added bool code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
     println!("Hello, world!");
     let x= 100;
-    println!("x {}", x)
+    println!("x {}", x);
+
+    let is_family = true;
+    if is_family {
+
+        println!("is: {}", is_family )
+    }
 }
