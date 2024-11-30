b287ab96e02c768e00a00fb997228eb250f45787
Akshaybagai52 <akshaybagai52@gmail.com>
2024-11-30 18:59:01 +0530

    feat: added struct code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
-fn main() {
-    println!("Hello, world!");
-    let x= 100;
-    println!("x {}", x);
+// fn main() {
+//     println!("Hello, world!");
+//     let x= 100;
+//     println!("x {}", x);
 
-    let is_family = true;
-    if is_family {
-        println!("is: {}", is_family )
-    }
+//     let is_family = true;
+//     if is_family {
+//         println!("is: {}", is_family )
+//     }
 
-    let sentence = String::from("My name is Akshay");
-    let first_word = get_first_word(sentence);
+//     let sentence = String::from("My name is Akshay");
+//     let first_word = get_first_word(sentence);
 
-    println!("first word : {}", first_word);
-    let n = 1000;
-    for i in 0..n {
-        println!("Hello: {}", i)
-    }
-}
+//     println!("first word : {}", first_word);
+//     let n = 1000;
+//     for i in 0..n {
+//         println!("Hello: {}", i)
+//     }
+// }
 
 fn get_first_word(sentence: String) -> String {
     let mut ans = String::from("");
@@ -27,5 +27,23 @@ fn get_first_word(sentence: String) -> String {
         }
     }
     return  ans;
+}
 
+struct Rect {
+    width: u32,
+    height: u32
+}
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    
+}
+
+fn main() {
+    let rect = Rect {
+        width: 32,
+        height: 50,
+    };
+    print!("The area of rectangle is {}", rect.area());
 }
